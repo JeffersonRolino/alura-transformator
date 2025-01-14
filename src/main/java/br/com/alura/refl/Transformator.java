49cf511b7a079715280a10b259d0d4a5aae9415d
jeffersonrolino <jefferson.rolino@gmail.com>
2025-01-14 13:15:55 -0300

    Capítulo 02 - Aula 04: Pegando campos de uma classe

--- src/main/java/br/com/alura/refl/Transformator.java
+++ src/main/java/br/com/alura/refl/Transformator.java
@@ -1,12 +1,16 @@
 package br.com.alura.refl;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class Transformator {
 
     public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<?> source = input.getClass();
-        Class<?> target = source.forName(source.getClass() + "DTO");
+        Class<?> target = Class.forName(source + "DTO");
+
+        Field[] sourceFields = source.getDeclaredFields();
+        Field[] targetFields = target.getDeclaredFields();
 
         return (O)target.getDeclaredConstructor().newInstance();
     }
