626412f8f12ceb27a2c1bb1e46f58dc87bca95ec
jeffersonrolino <jefferson.rolino@gmail.com>
2025-01-15 05:48:16 -0300

    Capítulo 04 - Aula 03: Teste shouldTransform

--- src/test/java/br/com/alura/refl/TransformatorTest.java
+++ src/test/java/br/com/alura/refl/TransformatorTest.java
@@ -1,4 +1,23 @@
 package br.com.alura.refl;
 
+import br.com.alura.Pessoa;
+import br.com.alura.PessoaDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
 public class TransformatorTest {
+
+    Pessoa pessoa = new Pessoa(1, "João", "1234");
+
+    @Test
+    public void shouldTransform() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        Transformator transformator = new Transformator();
+        PessoaDTO pessoaDTO = transformator.transform(pessoa);
+
+        Assertions.assertInstanceOf(PessoaDTO.class, pessoaDTO);
+        Assertions.assertEquals(pessoaDTO.getNome(), pessoa.getNome());
+        Assertions.assertEquals(pessoaDTO.getCpf(), pessoa.getCpf());
+    }
 }
